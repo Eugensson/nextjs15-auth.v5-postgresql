16ff5e3e2d484f49944290c289a530627fc6a0bd
Serhii Kulbashnyi <112683442+Eugensson@users.noreply.github.com>
2025-08-24 21:19:54 +0300

    Added callbackUrl in Socials component

--- app/(protected)/server/page.tsx
+++ app/(protected)/server/page.tsx
@@ -1,4 +1,6 @@
+import { Suspense } from "react";
 import { FaServer } from "react-icons/fa";
+import { BeatLoader } from "react-spinners";
 
 import { UserInfo } from "@/components/user-info";
 
@@ -7,7 +9,17 @@ import { currentUser } from "@/lib/auth";
 const ServerPage = async () => {
   const user = await currentUser();
 
-  return <UserInfo label="Server components" user={user} icon={FaServer} />;
+  return (
+    <Suspense
+      fallback={
+        <div className="flex justify-center items-center h-[80vh]">
+          <BeatLoader className="size-8 text-white/80" />
+        </div>
+      }
+    >
+      <UserInfo label="Server components" user={user} icon={FaServer} />
+    </Suspense>
+  );
 };
 
 export default ServerPage;
--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -1,4 +1,6 @@
+import { Suspense } from "react";
 import type { Metadata } from "next";
+import { BeatLoader } from "react-spinners";
 
 import { RegisterForm } from "@/components/auth/register-form";
 
@@ -7,7 +9,17 @@ export const metadata: Metadata = {
 };
 
 const RegisterPage = () => {
-  return <RegisterForm />;
+  return (
+    <Suspense
+      fallback={
+        <div className="flex justify-center items-center h-[80vh]">
+          <BeatLoader className="size-8 text-white/80" />
+        </div>
+      }
+    >
+      <RegisterForm />
+    </Suspense>
+  );
 };
 
 export default RegisterPage;
